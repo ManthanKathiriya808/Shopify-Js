53afc8a0e68fc677b75383e57248479e595072b6
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-01-01 15:32:14 +0530

    dag

--- js/script.js
+++ js/script.js
@@ -85,11 +85,24 @@ function showData(data){
                      
                       <h5 class="card-text text-danger">💲<span class="text-decoration-line-through">${ele.oldPrice}</span> ${ele.price}</h5>
         <div class="">
-        <h5>⭐⭐⭐⭐⭐</h5>
+        <h5>
+<div class="rating">
+  <input value="5" name="rating" id="star5" type="radio">
+  <label for="star5"></label>
+  <input value="4" name="rating" id="star4" type="radio">
+  <label for="star4"></label>
+  <input value="3" name="rating" id="star3" type="radio">
+  <label for="star3"></label>
+  <input value="2" name="rating" id="star2" type="radio">
+  <label for="star2"></label>
+  <input value="1" name="rating" id="star1" type="radio">
+  <label for="star1"></label>
+</div>
+</h5>
                      
         
           ${!checkData(ele._id) ? `  
-  <div class="btn-group" role="group" aria-label="Basic example">
+  <div class="btn-group w-100 mt-2" role="group" aria-label="Basic example">
   <button type="button" class="btn btn-dark btn-sm"  onclick="inCount(${ele._id})">
     <i class="ri-add-fill  "></i></button>
   <button type="button" class="btn btn-outline-dark disabled btn-sm">
